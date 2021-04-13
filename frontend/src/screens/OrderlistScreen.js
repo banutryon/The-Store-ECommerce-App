eb45e4d88e978529d689ebc8fa11697dd6f63603
banu tryon <banutryon@gmail.com>
2021-04-13 13:17:37 -0700

    added seller order page views and auth/admin access

--- frontend/src/screens/OrderlistScreen.js
+++ frontend/src/screens/OrderlistScreen.js
@@ -7,6 +7,7 @@ import MessageBox from "../components/MessageBox";
 import { ORDER_DELETE_RESET } from "../constants/orderConstants";
 
 export default function OrderlistScreen(props) {
+	const sellerMode = props.match.path.indexOf("/seller") >= 0;
 	const orderList = useSelector((state) => state.orderList);
 	const { loading, error, orders } = orderList;
 	const orderDelete = useSelector((state) => state.orderDelete);
@@ -15,12 +16,13 @@ export default function OrderlistScreen(props) {
 		error: errorDelete,
 		success: successDelete,
 	} = orderDelete;
-
+	const userSignin = useSelector((state) => state.userSignin);
+	const { userInfo } = userSignin;
 	const dispatch = useDispatch();
 	useEffect(() => {
 		dispatch({ type: ORDER_DELETE_RESET });
-		dispatch(listOrders());
-	}, [dispatch, successDelete]);
+		dispatch(listOrders({ seller: sellerMode ? userInfo._id : "" }));
+	}, [dispatch, sellerMode, successDelete, userInfo._id]);
 	const deleteHandler = (order) => {
 		if (window.confirm("Are You Sure.... Confirm Delete")) {
 			dispatch(deleteOrder(order._id));
