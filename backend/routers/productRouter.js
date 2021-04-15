ef0b4fad86b4174d0f1d08a42ed8f114d5f00664
banu tryon <banutryon@gmail.com>
2021-04-15 12:49:54 -0700

    added search bar and search screen

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -10,12 +10,14 @@ productRouter.get(
 	"/",
 	expressAsyncHandler(async (req, res) => {
 		try {
+			const name = req.query.name || "";
 			const seller = req.query.seller || "";
+			const nameFilter = name ? { name: { $regex: name, $options: "i" } } : {};
 			const sellerFilter = seller ? { seller } : {};
-			const products = await Product.find({ ...sellerFilter }).populate(
-				"seller",
-				"seller.name seller.logo"
-			);
+			const products = await Product.find({
+				...sellerFilter,
+				...nameFilter,
+			}).populate("seller", "seller.name seller.logo");
 			res.send(products);
 		} catch (error) {
 			res.send("you have an error on route '/' ");
