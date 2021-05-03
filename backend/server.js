a86c1bf87b31f969dd4da7115e0357b3b68ecbc5
banu tryon <banutryon@gmail.com>
2021-05-03 15:18:19 -0700

    added AWS to fix heroku upload issue

--- backend/server.js
+++ backend/server.js
@@ -15,15 +15,11 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-mongoose.connect(
-	// process.env.MONGODB_URL
-	mongodbURL,
-	{
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useCreateIndex: true,
-	}
-);
+mongoose.connect(mongodbURL, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+});
 app.use("/api/uploads", uploadRouter);
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
